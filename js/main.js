15ab67029b645fce813fa0bb4b0510012a83247e
Carlosaven <114002477+Carlosaven@users.noreply.github.com>
2023-08-10 15:29:02 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function abrirPDF() {
+  // Cambia "ruta/al/archivo.pdf" por la URL real de tu archivo PDF
+  var rutaPDF ="/img/CV_Carlos_DA_2023.pdf";
+  
+  // Abre una nueva ventana o pestaña del navegador con el PDF
+  window.open(rutaPDF, "_blank");
+}
+
